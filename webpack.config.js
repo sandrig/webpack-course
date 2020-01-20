0113c98f47874c71785c2be889cd0c47f3389a36
sandrig <sandrig.dev@gmail.com>
2020-01-20 20:39:25 +0300

    feat: add fonts

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,10 @@ module.exports = {
       {
         test: /\.(png|jpg|svg|gif)$/,
         use: ['file-loader']
+      },
+      {
+        test: /\.(ttf|woff|woff2|eot)$/,
+        use: ['file-loader']
       }
     ]
   }
