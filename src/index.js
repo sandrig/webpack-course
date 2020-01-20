06abba87ace80a60207cf6989e816c3b5233b53c
sandrig <sandrig.dev@gmail.com>
2020-01-20 19:25:49 +0300

    feat: load json example

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import { Post } from './Post';
+import json from './assets/json';
 import './styles/styles.css';
 
 const post = new Post('Webpack Post Title');
 
 console.log('Post to String', post.toString());
+
+console.log('JSON', json);
