29395ec11ba0b9af41fde1ebad53cb2a9b889323
sandrig <sandrig.dev@gmail.com>
2020-01-22 19:31:07 +0300

    feat: add webpack-dev-server

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import './styles/styles.css';
 
 const post = new Post('Webpack Post Title', WebpackLogo);
 
-$('pre').html(post.toString());
+$('pre').addClass('code').html(post.toString());
 
 // console.log('Post to String', post.toString());
 
--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,9 @@ module.exports = {
       chunks: 'all'
     }
   },
+  devServer: {
+    port: 4200
+  },
   plugins: [
     new HTMLWebpackPlugin({
       template: './index.html'
