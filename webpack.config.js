ae189082b530572c0749dae0346b066ed5a2006e
sandrig <sandrig.dev@gmail.com>
2020-01-24 22:51:06 +0300

    feat: optimization styles for development

--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,8 @@ const optimization = () => {
   return config;
 };
 
+const filename = ext => isDev ? `[name].${ext}` : `[name].[hash].${ext}`;
+
 module.exports = {
   context: path.resolve(__dirname, 'src'),
   mode: 'development',
@@ -35,7 +37,7 @@ module.exports = {
     analytics: './analytics.js'
   },
   output: {
-    filename: '[name].[contenthash].js',
+    filename: filename('js'),
     path: path.resolve(__dirname, 'dist')
   },
   resolve: {
@@ -65,7 +67,7 @@ module.exports = {
       }
     ]),
     new MiniCssExtractPlugin({
-      filename: '[name].[contenthash].css'
+      filename: filename('css')
     })
   ],
   module: {
