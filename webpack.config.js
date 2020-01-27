a5ee209bb5769eac626b8138992246bb848cd3c2
sandrig <sandrig.dev@gmail.com>
2020-01-27 21:21:49 +0300

    feat: add webpack-bundle-analyzer

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserWebpackPlugin = require('terser-webpack-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 const isDev = process.env.NODE_ENV === 'development';
 const isProd = !isDev;
@@ -79,6 +80,33 @@ const jsLoaders = () => {
   return loaders;
 };
 
+const plugins = () => {
+  const base = [
+    new HTMLWebpackPlugin({
+      template: './index.html',
+      minify: {
+        collapseWhitespace: isProd
+      }
+    }),
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin([
+      {
+        from: path.resolve(__dirname, 'src/favicon.ico'),
+        to: path.resolve(__dirname, 'dist'),
+      }
+    ]),
+    new MiniCssExtractPlugin({
+      filename: filename('css')
+    })
+  ];
+
+  if(isProd) {
+    base.push(new BundleAnalyzerPlugin())
+  }
+
+  return base;
+};
+
 module.exports = {
   context: path.resolve(__dirname, 'src'),
   mode: 'development',
@@ -103,24 +131,7 @@ module.exports = {
     hot: isDev
   },
   devtool: isDev ? 'source-map' : '',
-  plugins: [
-    new HTMLWebpackPlugin({
-      template: './index.html',
-      minify: {
-        collapseWhitespace: isProd
-      }
-    }),
-    new CleanWebpackPlugin(),
-    new CopyWebpackPlugin([
-      {
-        from: path.resolve(__dirname, 'src/favicon.ico'),
-        to: path.resolve(__dirname, 'dist'),
-      }
-    ]),
-    new MiniCssExtractPlugin({
-      filename: filename('css')
-    })
-  ],
+  plugins: plugins(),
   module: {
     rules: [
       {
